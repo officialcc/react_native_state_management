8a010bff023108ba5f3c98d8cb8b07ac9cd2aaa0
Colin Chin <xnnproject@yahoo.co.uk>
2026-03-13 00:47:09 +0800

    Completed Lesson #119

--- store/context/favorites-context.js
+++ store/context/favorites-context.js
@@ -1,13 +1,31 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const FavoritesContext = createContext({
+export const FavoritesContext = createContext({
     ids: [],
-    addFavorite: (id) => {},
-    removeFavorite: (id) => {},
+    addFavorite: (id) => { },
+    removeFavorite: (id) => { },
 });
 
-function FavoritesContextProvider({children}) {
-    return <FavoritesContext.Provider>{children}</FavoritesContext.Provider>
+function FavoritesContextProvider({ children }) {
+    const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+
+    function addFavorite(id) {
+        setFavoriteMealIds((currentFavoriteIds) => [...currentFavoriteIds, id]);
+    }
+
+    function removeFavorite(id) {
+        setFavoriteMealIds((currentFavoriteIds) =>
+            currentFavoriteIds.filter((mealId) => mealId !== id)
+        );
+    }
+
+    const value = {
+        ids: favoriteMealIds,
+        addFavorite: addFavorite,
+        removeFavorite: removeFavorite,
+    }
+
+    return <FavoritesContext.Provider value={value}>{children}</FavoritesContext.Provider>
 }
 
 export default FavoritesContextProvider;
